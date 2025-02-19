a1fa187a28abaf16fe0faa8839ff48dd34ba5029
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-02-19 08:34:17 -0300

    Adicionando estudo sobre tipos primitivos parte 1

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 22;
+
+        System.out.println("A idade é " + idade + " anos");
+    }
+}
