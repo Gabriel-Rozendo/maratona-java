206ca710a5c28b04abc58de20971b51563c3e4ea
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-02-19 12:42:27 -0300

    Adicionando estudo sobre casting

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,8 +3,20 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 22;
+
+        int idade = (int)10000000000000L; // Fazendo um casting
+
+        long numeroGrande = 1000000000L; // Usar L no final do long
+        double salarioDouble = 2000;
+        float salarioFloat = 2500.0F; // Usar F no final de um float
+        byte idadeByte = 10;
+        short idadeShort = 32000;
+        boolean isVerdadeiro = true;
+        boolean isFalso = false;
+        char caractere = 65;
 
         System.out.println("A idade é " + idade + " anos");
+        System.out.println(isVerdadeiro);
+        System.out.println(caractere);
     }
 }
