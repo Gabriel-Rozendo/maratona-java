575c984266b55a552d2c638ad67841bdda0c6dc2
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-02-20 15:51:26 -0300

    Adicionando estudos sobre operadores aritméticos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+
+        // Operadores aritmeticos: + - * / %
+
+        int numero1 = 10;
+        int numero2 = 20;
+
+        System.out.println(numero2 + numero1);
+    }
+}
