4d62fe178739fa1172d4d29156b3b1a4d1c1483c
Gabriel Rozendo <gabrielrozendodacosta@gmail.com>
2025-02-21 09:01:45 -0300

    Adicionando estudos sobre operadores lógicos

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -9,5 +9,21 @@ public class Aula04Operadores {
         int numero2 = 20;
 
         System.out.println(numero2 + numero1);
+
+        int resto = 21 % 2; // Se o resto for 0 o número é par
+        System.out.println(resto);
+
+
+        // Operadores lógicos: < / > / <= / >= / == / !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualAVinte = 10 == 20;
+        boolean isDezDiferenteDeVinte = 10 != 20;
+
+        System.out.println("isDezMaiorQueVinte: " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte: " + isDezMenorQueVinte);
+        System.out.println("isDezIgualAVinte: " + isDezIgualAVinte);
+        System.out.println("isDezDiferenteDeVinte: " + isDezDiferenteDeVinte);
     }
 }
