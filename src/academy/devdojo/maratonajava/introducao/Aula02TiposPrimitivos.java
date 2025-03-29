f9cc4dd05c76491d710d7554317348a80f0c2835
Gabriel <gabrielrozendodacosta@gmail.com>
2025-03-29 20:09:16 -0300

    feat: Adiciona estudo sobre tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,16 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int idade = 10;
-        System.out.println("Idade: " + idade);
+        long numeroGrande = 10000000000L;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500F;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println(verdadeiro);
+
     }
 }
