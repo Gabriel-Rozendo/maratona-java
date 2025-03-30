5ce0cfe6b4285fff61df80013a80d02ad18fbf70
Gabriel <gabrielrozendodacosta@gmail.com>
2025-03-30 09:56:24 -0300

    feat: Adiciona estudo sobre operadores

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -23,5 +23,41 @@ public class Aula04Operadores {
         boolean isDezMenorQueVinte = 10 < 20;
         System.out.println("isDezMaiorQueVinte: " + isDezMaiorQueVinte);
         System.out.println("isDezMenorQueVinte: " + isDezMenorQueVinte);
+
+        // Operadores lógicos: AND (&&) / OR (||) / NOT (!)
+
+        int idade = 29;
+        double salario = 3500;
+
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario > 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta: " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta: " + isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        double valorPlaystation = 5000;
+
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel: " + isPlaystationCincoCompravel);
+
+        // Operadores de Atribuição: =  +=  -=  *=  /=  %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.println(bonus);
+
+        // Incremento: ++
+        // Decremento: --
+
+        int contador = 0;
+        contador++;
+        System.out.println(contador);
+        contador--;
+        System.out.println(contador);
     }
 }
